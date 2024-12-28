46118213ca4a31feaa168d7d28ab008cf3471cd5
lachiejames <lachie_james@hotmail.com.au>
2024-12-29 01:49:28 +1100

    chore(cdk): upgrade PostgreSQL engine version for Eliza RDS instance from 13.18 to 15.10

--- packages/cdk/src/aws/rds/createElizaRDSInstance.ts
+++ packages/cdk/src/aws/rds/createElizaRDSInstance.ts
@@ -20,7 +20,7 @@ export const createElizaRDSInstance = ({
         }),
         databaseName: "eliza",
         engine: rds.DatabaseInstanceEngine.postgres({
-            version: rds.PostgresEngineVersion.VER_13_18,
+            version: rds.PostgresEngineVersion.VER_15_10,
         }),
         instanceType: ec2.InstanceType.of(
             ec2.InstanceClass.T4G,
