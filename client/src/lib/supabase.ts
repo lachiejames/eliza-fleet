51b78f2c81156271a68b905f326f2af1db11c6fe
lachiejames <lachie_james@hotmail.com.au>
2024-12-30 08:01:41 +1100

    feat(client): enhance authentication flow and add error handling

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,19 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr'
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
 }
 if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
 }
 
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  {
-    auth: {
-      autoRefreshToken: true,
-      persistSession: true,
-    },
-  }
-);
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
